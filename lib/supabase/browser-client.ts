1f63cf21d128fcc3484505ece2a1cae33895420e
systemi-i <federico@i-i.earth>
2025-05-15 17:03:29 +0100

    Update browser-client.ts

--- lib/supabase/browser-client.ts
+++ lib/supabase/browser-client.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export const supabase = createBrowserClient(
-  "https://your-project-name.supabase.co", // ⬅️ Your real Supabase project URL
-  "eyJhbGciOi..." // ⬅️ Your real Supabase anon public key
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
